e5d3480b04fec7a89791ea156ed429b380c27181
nmathiou <nick_mathiou@hotmail.com>
2022-10-29 15:40:29 +0300

    to lap

--- main.py
+++ main.py
@@ -1,34 +1,23 @@
+from src.base.individual import Individual
+from src.population import Population
 
 
 
 
 
+def fitness_func(params):
+    return params[0]**2 - params[1]**3 + params[2]**2 + params[3]**3
 
 
 
+if __name__ == "__main__":
+    pop = Population(pop_size = 200, parameters_bounds=[[-1, 1], [1, 2], [3, 12], [-3, -1]], fitness_function=fitness_func)
 
+    pop.initialize()
 
+    pop.calculate_fitness()
 
+    print(pop.avg_fitness())
+    print(pop.get_best_individuals()[0].fitness)
 
-
-
-
-
-
-
-
-a = [1, 2, 3]
-b = [4, 5, 6]
-
-c = [a, b]
-
-d = c.copy()
-
-d[0][0] = 0
-
-print(c)
-
-
-
-
-
+    print(1 - 1 + 12**2 + -1**3)
--- src/population.py
+++ src/population.py
@@ -1,4 +1,4 @@
-from base.individual import Individual
+from src.base.individual import Individual
 import numpy as np
 
 
@@ -7,21 +7,21 @@ import numpy as np
 
 class Population:
     
-    def __init__(self, pop_size = 100, parameters_bounds=[[-1, 1], [1, 2], [3, 12], [-3, -1]]):
+    def __init__(self, pop_size = 100, parameters_bounds=[[-1, 1], [1, 2], [3, 12], [-3, -1]], fitness_function = None):
         self.pop_size = pop_size
         self.parameters_size = len(parameters_bounds)
         self.upper_bounds = np.array([x[1] for x in parameters_bounds])
         self.lower_bounds = np.array([x[0] for x in parameters_bounds])
         self.population = []
+        self.is_sorted = False
+        self.fitness_function = fitness_function
     
-    
-    
+
     def initialize(self):
         
         random_parameters = np.random.rand(self.pop_size, self.parameters_size)*(self.upper_bounds-self.lower_bounds)+self.lower_bounds
-    
         for i in range(self.pop_size):
-            self.population.append(Individual(params=random_parameters))
+            self.population.append(Individual(params=random_parameters[i]))
     
 
     def avg_fitness(self):
@@ -31,19 +31,51 @@ class Population:
 
         for i in self.population:
             avg_fit += i.fitness
-            if i.fitness > best_fit:
-                best_fit = i.fitness
-                best_individual = i
+
                 
         return avg_fit/self.pop_size
     
     
-    def get_best_individuals(self, num_of_bests):
+    def get_best_individuals(self, num_of_bests=1):
+        bests = []
+        if not(self.is_sorted):
+            self.__sort()
+
+        for i in range(num_of_bests):
+            bests.append(self.population[i])
+        return bests
+
+
+    def calculate_fitness(self):
+        for ind in self.population:
+            ind.fitness = self.fitness_function(ind.parameters)
+
+
+    def crossover(self, parent_1: Individual, parent_2: Individual):
         
-    
-    
-    
-    
+
+    def __sort(self):
+
+        check_is_sorted = 0
+        self.is_sorted = False
+
+        while check_is_sorted < len(self.population)-1:
+            check_is_sorted = 0
+            for i in range(len(self.population)-1):
+                check_is_sorted += 1
+                if self.population[i].fitness < self.population[i+1].fitness:
+                    temp = self.population[i]
+                    self.population[i] = self.population[i+1]
+                    self.population[i+1] = temp
+                    check_is_sorted = 0
+        
+        self.is_sorted = True
+
+
+
+
+
+
     
 if __name__ == "__main__":
 
